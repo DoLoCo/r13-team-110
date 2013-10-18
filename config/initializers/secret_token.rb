ff2657f56f0cf46264544c6441b4ed8884802e89
Scott Pullen <s.pullen05@gmail.com>
2013-10-18 19:41:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CollabBrainstorming::Application.config.secret_key_base = 'ca2a0967df3f4137c19c6b173aa582583e38f027df806ddb4a51df56dcdcee9c7bd42242ef42ab9e26bb34929301eb53e151198a3973546ba7283c153884707e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CollabBrainstorming::Application.config.session_store :cookie_store, key: '_collab_brainstorming_session'
