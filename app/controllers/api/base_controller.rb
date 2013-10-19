1671d330a6cbb77abedb30b59f121b3c4df887cd
Scott Pullen <s.pullen05@gmail.com>
2013-10-19 12:18:45 -0400

    respond with json 404 for records that are not found for the api

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,3 +1,7 @@
 class Api::BaseController < AuthenticatedController
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: [], status: :not_found
+  end
+
   respond_to :json
 end
