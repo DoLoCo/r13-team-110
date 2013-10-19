1c0d19198c9c6fc9cff979fbf108a8f1c94f851f
Scott Pullen <s.pullen05@gmail.com>
2013-10-19 00:38:24 -0400

    redirect to home from welcome if logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,8 @@
 class WelcomeController < ApplicationController
+  before_action :redirect_to_home
   def index; end;
+private
+  def redirect_to_home
+    redirect_to home_index_url if user_signed_in?
+  end
 end
