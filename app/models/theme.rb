089c29d77cec872f0b63c274b609c5b2d7c40dbd
Scott Pullen <s.pullen05@gmail.com>
2013-10-18 23:37:06 -0400

    adding validation to the theme model

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -2,4 +2,7 @@ class Theme < ActiveRecord::Base
   has_many :theme_members
   has_many :users, through: :theme_members
   has_many :ideas
+
+  validates :title, presence: true
+  validates :description, presence: true
 end
