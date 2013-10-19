2060c81eb9ea6a9a1c4e89db3c68f5a6b3908ee4
Scott Pullen <s.pullen05@gmail.com>
2013-10-18 22:50:29 -0400

    setting the approriated layouts

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,10 +1,10 @@
 class ThemesController < ApplicationController
   respond_to :html
-  
+
   before_action :load_theme, only: [:show]
 
   def show
-    respond_with @theme
+    respond_with @theme, layout: 'theme'
   end
 
   def new
@@ -13,6 +13,7 @@ class ThemesController < ApplicationController
 
   def create
     @theme = Theme.new(permitted_params.theme)
+    # TODO create the initial theme member for the user that created it
     respond_with @theme
   end
 
