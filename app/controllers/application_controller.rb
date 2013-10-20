7a96e127ec8bd2947a6e24b030db12ab6ac9da3e
Scott Pullen <s.pullen05@gmail.com>
2013-10-20 00:27:27 -0400

    whoops forgot about the redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    home_url
+    themes_url
   end
 
   def permitted_params
