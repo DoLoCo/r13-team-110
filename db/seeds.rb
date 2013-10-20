f9461052e9a5e032a114224544041b94147dde1b
Scott Pullen <s.pullen05@gmail.com>
2013-10-20 19:01:39 -0400

    adding a guest user seed account

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(
+  name: 'Guest User',
+  email: User::GUEST_USER_EMAIL,
+  password: 'guestP455w0rd',
+  password_confirmation: 'guestP455w0rd'
+)
